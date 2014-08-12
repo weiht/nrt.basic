960c8e40d545fbac6e28eace2b8de609a028f61f
weiht <weihthchk@gmail.com>
2014-08-12 22:05:36 +0800

    DataAccess CRUD methods added.

--- src/main/groovy/data/DataAccess.groovy
+++ src/main/groovy/data/DataAccess.groovy
@@ -1,5 +1,58 @@
-package data
+package data;
+
+import org.nutz.dao.Dao;
+import org.necros.webmvc.util.*;
+
+import sys.ObjectBasics;
+import sys.Touchable;
 
 class DataAccess {
+	private static Dao dao;
+	private static SessionUtil sessionUtil;
 
+	static synchronized DataAccess getInstance(def applicationContext) {
+		if (dao == null) {
+			dao = (Dao) applicationContext.getBean('dao');
+			sessionUtil = (SessionUtil) applicationContext.getBean('sessionUtil');
+		}
+		return new DataAccess();
+	}
+
+	private String currentLogin() {
+		String uid = sessionUtil.currentLogin;
+		if (uid == null) uid = 'anonymous';
+		return uid;
+	}
+
+	Object create(Object data) {
+		if (data instanceof ObjectBasics) {
+			data.id = UUID.randomUUID().toString();
+		}
+		if (data instanceof Touchable) {
+			data.creatorId = currentLogin();
+			data.createTime = new Date();
+			//TODO Record touch log
+		}
+		dao.insert(data);
+		return data;
+	}
+
+	Object read(Class<?> clazz, String id) {
+		return dao.fetch(clazz, id);
+	}
+
+	Object update(Object data) {
+		//TODO Read data and only modify properties that are modifiable.
+		if (data instanceof Touchable) {
+			data.updaterId = currentLogin();
+			data.updateTime = new Date();
+			//TODO Record touch log
+		}
+		dao.update(data);
+		return data;
+	}
+
+	Object delete(Object data) {
+		dao.delete(data);
+	}
 }
